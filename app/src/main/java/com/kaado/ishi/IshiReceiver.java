cbcbf4a6e7bbdb07856bd32f9adfefc9bf575b40
Taylor Foxhall <lxmachina@gmail.com>
2015-06-23 20:59:49 -0400

    Fixed up mass commenting

--- app/src/main/java/com/kaado/ishi/IshiReceiver.java
+++ app/src/main/java/com/kaado/ishi/IshiReceiver.java
@@ -1,6 +1,7 @@
 package com.kaado.ishi;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.PebbleKit.PebbleDataReceiver;
@@ -9,7 +10,9 @@ import com.getpebble.android.kit.util.PebbleDictionary;
 import java.util.UUID;
 
 /**
- * Created by taylor on 6/23/15.
+ *  Author: Taylor (hallfox)
+ *  Does all the heavy lifting. Watches for broadcast events from Ishi and interacts with Anki
+ *  to get the requested data or post an answer to a flashcard.
  */
 public class IshiReceiver extends PebbleDataReceiver
 {
@@ -53,73 +56,26 @@ public class IshiReceiver extends PebbleDataReceiver
     {
         PebbleKit.sendAckToPebble(context, transactionId);
 
-//        if (data.getString(KEY_DECKS) != null)
-//        {
-//            final String deck = data.getString(KEY_DECKS);
-//            long did = getCol().getDecks().byName(deck).optLong("id");
-//            getCol().getDecks().select(did);
-//            Timber.i("Selected deck: " + deck + " with id: " + did);//getCol().getDecks().byName(deck));
-//            Timber.i("Sending question to pebble...");
-//            mCard = getCol().getSched().getCard();
-//            PebbleDictionary q = new PebbleDictionary();
-//            q.addString(KEY_QUESTION, Jsoup.parse(mCard.qSimple()).text());
-//            PebbleKit.sendDataToPebble(context, WATCHAPP_UUID, q);
-//        }
-//
-//        if (data.getInteger(KEY_EASE) != null)
-//        {
-//            final int ease = data.getInteger(KEY_EASE).intValue();
-//            getCol().getSched().answerCard(mCard, ease);
-//            Timber.i("Sending question to pebble...");
-//            mCard = getCol().getSched().getCard();
-//            PebbleDictionary q = new PebbleDictionary();
-//            q.addString(KEY_QUESTION, Jsoup.parse(mCard.qSimple()).text());
-//            PebbleKit.sendDataToPebble(context, WATCHAPP_UUID, q);
-//        }
-//
-//        if (data.getInteger(KEY_ACTION) != null)
-//        {
-//            final int action = data.getInteger(KEY_ACTION).intValue();
-//            Timber.d("Found an action intent: " + action);
-//            switch (action)
-//            {
-//                case ACTION_ANS:
-//                    PebbleDictionary a = new PebbleDictionary();
-//                    a.addString(KEY_ANSWER, Jsoup.parse(mCard.aSimple()).text());
-//                    PebbleKit.sendDataToPebble(context, WATCHAPP_UUID, a);
-//                    break;
-//                case ACTION_EASE:
-//                    int eases = getCol().getSched().answerButtons(mCard);
-//                    PebbleDictionary e = new PebbleDictionary();
-//                    e.addInt32(KEY_EASE, eases);
-//                    Timber.i("Display " + eases + " ease levels.");
-//                    PebbleKit.sendDataToPebble(context, WATCHAPP_UUID, e);
-//                    break;
-//                case ACTION_Q:
-//                    Timber.i("Sending question to pebble...");
-//                    mCard = getCol().getSched().getCard();
-//                    PebbleDictionary q = new PebbleDictionary();
-//                    q.addString(KEY_QUESTION, Jsoup.parse(mCard.qSimple()).text());
-//                    PebbleKit.sendDataToPebble(context, WATCHAPP_UUID, q);
-//                    break;
-//                case ACTION_DECK_SELECT:
-//                    Timber.i("Asking for decks...");
-//                    ArrayList<String> strs = getCol().getDecks().allNames();
-//                    PebbleDictionary message = new PebbleDictionary();
-//
-//                    String msg = "";
-//                    for (String s : strs)
-//                    {
-//                        if (s.equals("Default")) continue;
-//                        msg += s + ";";
-//                    }
-//                    message.addString(KEY_DECKS, msg);//strs.get(0));// + ";" + strs.get(1));
-//                    //Timber.i("Send a string of size: " + strs.get(0).length());
-//                    PebbleKit.sendDataToPebble(context, WATCHAPP_UUID, message);
-//                    break;
-//                default:
-//                    Log.d(TAG, "Unrecognized action.");
-//            }
-//        }
+        // Handle incoming request for getting the user's decks
+        if (data.getString(KEY_DECKS) != null)
+        {
+            final String deck = data.getString(KEY_DECKS);
+        }
+        // Handles answer to the current card and sends out a new question
+        if (data.getInteger(KEY_EASE) != null)
+        {
+            final int ease = data.getInteger(KEY_EASE).intValue();
+        }
+        // Handles general action
+        // Originally handled multiple situations, now does nada
+        if (data.getInteger(KEY_ACTION) != null)
+        {
+            final int action = data.getInteger(KEY_ACTION).intValue();
+            switch (action)
+            {
+                default:
+                    Log.d(TAG, "Unrecognized action.");
+            }
+        }
     }
 }
